7d751470dbd9f81cc51484e36397a71dc0f0d0d6
Lorran Sutter <sutterlorran@gmail.com>
2020-06-11 22:33:04 -0400

    Start download

--- src/components/Download/index.js
+++ src/components/Download/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Layout, Card } from '@shopify/polaris';
 
+import ipfs from '../../ipfs';
+
 const Download = () => {
+
+    (async () => {
+        for await (const fileIPFS of ipfs.get('QmWW8VUDVYmLAEP2rDJSUmLLjzhYBPAzgxD7kUkpj3bTaq')) {
+            console.log(fileIPFS);
+        }
+    })();
+
     return (
         <Layout.Section>
             <Card sectioned title="Download file">
