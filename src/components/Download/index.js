271043338cd6e79c65548cd88c386c2b3b08c3a6
Lorran Sutter <sutterlorran@gmail.com>
2020-06-13 11:47:48 -0400

    Finish download

--- src/components/Download/index.js
+++ src/components/Download/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Layout, Card } from '@shopify/polaris';
-
-import ipfs from '../../ipfs';
+import React, { useState, useCallback } from 'react';
+import { Layout, Card, TextField, FormLayout, Button } from '@shopify/polaris';
 
 const Download = () => {
 
-    (async () => {
-        for await (const fileIPFS of ipfs.get('QmWW8VUDVYmLAEP2rDJSUmLLjzhYBPAzgxD7kUkpj3bTaq')) {
-            console.log(fileIPFS);
-        }
-    })();
+    const [hash, setHash] = useState();
+
+    const handleChange = useCallback(hash => setHash(hash), []);
 
     return (
         <Layout.Section>
             <Card sectioned title="Download file">
+                <FormLayout>
+                    <TextField label="Input hash" placeholder="Hash" value={hash} onChange={handleChange} />
+                    <a
+                        href={`http://127.0.0.1:8080/ipfs/${hash}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ textDecoration: 'none' }}
+                        download
+                    >
+                        <Button primary>Download</Button>
+                    </a>
+                </FormLayout>
             </Card>
         </Layout.Section>
     );
